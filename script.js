7d4072ff99c35d638b5497d3660a71ebbddbb125
oshrag <35432980+oshrag@users.noreply.github.com>
2018-09-21 13:52:55 +0300

    "add go to tree" (#2)
    * add Go To Tree
    * style: delete spceas and commented code

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
+    
+	var arTree = $( ".tree" ).toArray();
+	var treeX = arTree[0].offsetLeft;
+	var treeY = arTree[0].offsetTop;
+	
 	$("#run").click(function(){
 		var noam = $(".noam");
+		
+		var arNoam = $( ".noam" ).toArray();
+		var noamWentX = arNoam[0].offsetLeft;
+		var noamWentY = arNoam[0].offsetTop;
 
 		var code = $("#code").val().split("\n");
-
+		
 		// remove spaces:
 		code = code.map(line => line.trim());
 
@@ -12,24 +21,45 @@ $(document).ready(function(){
 
 		// split each code line to words
 		code = code.map(line=>line.split(" "));
-
+	   
 		code.forEach(function (command){
 			if(command[0] === "לך") {
 				if(command[2] === "ימינה"){
+				    noamWentX += parseInt(command[1]);
+					
 					noam = noam.animate({
 						"left": "+=" + command[1] + "px"
 					}, 2000)
 				} else if(command[2] === "שמאלה"){
+				    noamWentX -= parseInt(command[1]);
+					
 					noam = noam.animate({
 						"left": "-=" + command[1] + "px"
 					}, 2000)
 				} else if(command[2] === "למטה"){
+				    noamWentY += parseInt(command[1]);
+					
 					noam = noam.animate({
 						"top": "+=" + command[1] + "px"
 					}, 2000)
 				} else if(command[2] === "למעלה"){
+				    noamWentY -= parseInt(command[1]);
+					
 					noam = noam.animate({
 						"top": "-=" + command[1] + "px"
+					}, 2000)
+				} 
+				else if(command[1] === "לעץ"){
+				
+                    var goLeft = treeX - noamWentX ;
+                    var goTop = treeY - noamWentY ;
+				 	console.log("goLeft="+goLeft);
+				 	console.log("goTop="+goTop);
+					
+					noam = noam.animate({
+						"left": "+=" + goLeft + "px"
+					}, 2000).noam = noam.animate({
+						"top": "+=" + goTop + "px"
 					}, 2000)
 				} 
 			} else if(command[0] === "תן") {
